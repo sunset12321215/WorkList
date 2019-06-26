72c558ea390ebd1143ece2a0fff0e7c8d15dc1eb
Sun <sunsetno08@gmail.com>
2019-06-26 10:16:27 +0700

    Initial Commit

---
+++ WorkList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkList
+//
+//  Created by CuongVX-D1 on 6/26/19.
+//  Copyright © 2019 CuongVX-D1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
